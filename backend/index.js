c8b3ca8d73ba80c8eff94dc2af18ac874ba2ac1c
Your Name <your.email@example.com>
2025-09-17 11:07:40 -0400

    Added dotenv and pg libraries and tested db connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    databse: process.env.DB_NAME
+})
 const app = express();
 const PORT = 3001;
 
 app.get('/', (req, res) => {
     res.send('Hello, World!')
+});
+
+app.get('/testdb', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json(result.rows);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).send('Error connecting to the db');
+    }
 });
 
 app.listen(PORT, () => {
