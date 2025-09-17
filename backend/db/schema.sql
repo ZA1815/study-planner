184aacbd72001414b2e0c90132537f18799218b5
Your Name <your.email@example.com>
2025-09-16 23:33:10 -0400

    Set up tables in db and tested running server

---
+++ backend/db/schema.sql
@@ -0,0 +1,28 @@
+-- Creates the users table
+CREATE TABLE users (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_name VARCHAR(30) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Creates the courses table
+CREATE TABLE courses (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Creates the assignments table
+CREATE TABLE assignments (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    course_id INT NOT NULL REFERENCES courses(id),
+    name VARCHAR(255) NOT NULL,
+    due_date TIMESTAMP,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
