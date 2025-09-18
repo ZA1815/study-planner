96eab5fdc4a57068c788aaa3f35aa66ae655e107
Your Name <your.email@example.com>
2025-09-17 21:56:41 -0400

    feat: Created barebones register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,4 +1,39 @@
+import { useState } from 'react';
+
 function RegisterPage() {
-    return <div>Register Page</div>;
+    const [userInfo, setUserInfo] = useState({username: '', email: '', password: ''});
+    
+    const onChangeFunc = (e) => {
+        setUserInfo({
+            ...userInfo,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const onSubmitFunc = (e) => {
+        e.preventDefault();
+        console.log('Form submitted with:', userInfo);
+    }
+
+    return (
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={onSubmitFunc}>
+                <div>
+                    <label>Username: </label>
+                    <input type="text" placeholder="johndoe123" name="username" value={userInfo.username} onChange={onChangeFunc} required />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input type="email" placeholder="johndoe@example.com" name="email" value={userInfo.email} onChange={onChangeFunc} required />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="password" name="password" value={userInfo.password} onChange={onChangeFunc} required />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
 }
 export default RegisterPage;
