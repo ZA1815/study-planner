2188781f403c6de29ac481533a513274a8158899
Your Name <your.email@example.com>
2025-09-18 23:24:32 -0400

    feat: Add login feature and add navbar changes to reflect login state

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,23 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+
 function Navbar () {
-    return <div>Navbar</div>;
+    const {token} = useContext(AuthContext);
+
+    return(
+        <nav>
+            <Link to="/"><h1>Study Planner</h1></Link>
+            <div>
+                {token ? (<button>Logout</button>)
+                : 
+                (<>
+                <Link to="/login">Login</Link>
+                <Link to="/register">Sign-up</Link>
+                </>)}
+            </div>
+        </nav>
+    );
 }
+
 export default Navbar;
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,14 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { AuthContext } from '../context/AuthContext';
 
 function LoginPage() {
     const [userInfo, setUserInfo] = useState({identifier: '', password: ''});
+
+    const {setToken} = useContext(AuthContext);
+
+    const nav = useNavigate();
 
     const onChangeFunc = (e) => {
         setUserInfo({
@@ -10,9 +17,23 @@ function LoginPage() {
         });
     };
 
-    const onSubmitFunc = (e) => {
+    const onSubmitFunc = async (e) => {
         e.preventDefault();
-        console.log('Form submitted with:', userInfo);
+        
+        try {
+            const url = 'http://localhost:3001/api/users/login'
+
+            const response = await axios.post(url, userInfo);
+            const genToken = response.data.token;
+
+            setToken(genToken);
+            alert('Login successful.');
+            nav('/');
+        }
+        catch (err) {
+            console.error(err.response.data);
+            alert('Login failed: ' + err.response.data.error);
+        }
     }
 
     return (
