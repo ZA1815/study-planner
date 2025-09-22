a40441575cd77317491263fb38d8acc9ba057186
ZA1815 <zaahme18@gmail.com>
2025-09-22 15:33:32 -0400

    feat: Add link to cloud db

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,22 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
+let pool;
+
+if (process.env.DATABASE_URL) {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+else {
+    pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     databse: process.env.DB_NAME
-});
+    });
+}
 
 module.exports = pool;
